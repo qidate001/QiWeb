f2055dbda3a95683ff2a862f003ac983bcfcc095
qidate001 <qidate001@gmail.com>
2026-07-17 20:41:40 +0800

    refactor(config, qistoolkit3): 现在齐的工具包3版本号与下载渠道等由js/config.js统一，而非硬编码

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// js/config.js
+const APP_CONFIG = {
+    version: 'v2.9.1',
+    
+    // 版本相关 URL
+    urls: {
+        release: 'https://github.com/qidate001/QisToolkit3/releases/tag/v2.9.1',
+        fullExe: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.1/QisToolkit3-v2.9.1-Full.exe',
+        fullRar: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.1/QisToolkit3-v2.9.1-Full.rar',
+        liteExe: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.1/QisToolkit3-v2.9.1-Lite.exe',
+        liteRar: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.1/QisToolkit3-v2.9.1-Lite.rar',
+        github: 'https://github.com/qidate001/QisToolkit3/releases',
+        license: 'https://github.com/qidate001/QisToolkit3/blob/main/LICENSE',
+        baiduPan: 'https://pan.baidu.com/s/1XTg3X1aTYdIP1Q11YZ0fiQ?pwd=qinb',
+        eula: '/docs/QisToolkit3_EULA.html'
+    }
+};
