5e1612b3441e680ffe2ce69e335c6aa1ce7dd977
qidate001 <qidate001@gmail.com>
2026-08-11 00:37:51 +0800

    config: 修改了齐的工具包相关配置

--- js/config.js
+++ js/config.js
@@ -1,14 +1,14 @@
 // js/config.js
 const APP_CONFIG = {
-    version: 'v2.9.1',
+    version: 'v2.9.2',
     
     // 版本相关 URL
     urls: {
-        release: 'https://github.com/qidate001/QisToolkit3/releases/tag/v2.9.1',
-        fullExe: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.1/QisToolkit3-v2.9.1-Full.exe',
-        fullRar: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.1/QisToolkit3-v2.9.1-Full.rar',
-        liteExe: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.1/QisToolkit3-v2.9.1-Lite.exe',
-        liteRar: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.1/QisToolkit3-v2.9.1-Lite.rar',
+        release: 'https://github.com/qidate001/QisToolkit3/releases/tag/v2.9.2',
+        fullExe: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.2/QisToolkit3-v2.9.2-Full.exe',
+        fullRar: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.2/QisToolkit3-v2.9.2-Full.rar',
+        liteExe: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.2/QisToolkit3-v2.9.2-Lite.exe',
+        liteRar: 'https://github.com/qidate001/QisToolkit3/releases/download/v2.9.2/QisToolkit3-v2.9.2-Lite.rar',
         github: 'https://github.com/qidate001/QisToolkit3/releases',
         license: 'https://github.com/qidate001/QisToolkit3/blob/main/LICENSE',
         baiduPan: 'https://pan.baidu.com/s/1XTg3X1aTYdIP1Q11YZ0fiQ?pwd=qinb',
